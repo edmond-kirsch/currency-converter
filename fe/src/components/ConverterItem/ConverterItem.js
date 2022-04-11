ebb6a4890d74a05690eb5fa8f16cb72b931d2cad
Pavel Arzhanov <arzhanovpavel1@gmail.com>
2022-04-11 12:31:48 +0300

    fix: remove 0 at the start of the input

--- fe/src/components/ConverterItem/ConverterItem.js
+++ fe/src/components/ConverterItem/ConverterItem.js
@@ -1,10 +1,11 @@
 import './ConverterItem.css';
 
 export default function ConverterItem({ currencyInfo, rate, setBase, setCurrencies, setValue }) {
+  let view = rate.toString().replace(/^0(?!\.|$)/, '');
   const handleInput = e => {
     setCurrencies((prevState) => {
       const result = {...prevState};
-      result.rates[currencyInfo.abbr] = +e.target.value
+      result.rates[currencyInfo.abbr] = +e.target.value;
       return result;
     })
     setValue(+e.target.value)
@@ -14,7 +15,7 @@ export default function ConverterItem({ currencyInfo, rate, setBase, setCurrenci
   return (
     <div className="converter-item">
       <label className="converter-item__label" htmlFor="">{currencyInfo.abbr}</label>
-      <input className="converter-item__input" type="number" value={rate} onInput={handleInput}/>
+      <input className="converter-item__input" type="number" value={view} onInput={handleInput}/>
       <div className="converter-item__description">{currencyInfo.description}</div>
     </div>
   )
