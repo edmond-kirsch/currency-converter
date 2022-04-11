12d48e59205b7570132ed1c88817e68af894a9cc
Pavel Arzhanov <arzhanovpavel1@gmail.com>
2022-04-11 08:56:34 +0300

    feat: prepare currencies descriptions

---
+++ fe/src/utils/currencies.js
@@ -0,0 +1,224 @@
+const CURRENCIES = [
+  {
+    abbr: 'USD',
+    description: 'US Dollar',
+    id: 1
+  },
+  {
+    abbr: 'ALL',
+    description: 'Albanian lek',
+    id: 2
+  },
+  {
+    abbr: 'AMD',
+    description: 'Armenian dram',
+    id: 3
+  },
+  {
+    abbr: 'AOA',
+    description: 'Angolan Kwanza',
+    id: 4
+  },
+  {
+    abbr: 'ARS',
+    description: 'Argentine Peso',
+    id: 5
+  },
+  {
+    abbr: 'AUD',
+    description: 'Australian Dollar',
+    id: 6
+  },
+  {
+    abbr: 'AWG',
+    description: 'Aruban florin',
+    id: 7
+  },
+  {
+    abbr: 'AZN',
+    description: 'Azerbaijan Manat',
+    id: 8
+  },
+  {
+    abbr: 'AZN',
+    description: 'Bosnian Convertible Mark',
+    id: 9
+  },
+  {
+    abbr: 'BBD',
+    description: 'Barbadian dollar',
+    id: 10
+  },
+  {
+    abbr: 'BDT',
+    description: 'Bangladeshi Taka',
+    id: 11
+  },
+  {
+    abbr: 'BGN',
+    description: 'Bulgarian Lev',
+    id: 12
+  },
+  {
+    abbr: 'BHD',
+    description: 'Bahraini dinar',
+    id: 13
+  },
+  {
+    abbr: 'BIF',
+    description: 'Burundian franc',
+    id: 14
+  },
+  {
+    abbr: 'BMD',
+    description: 'Bermudian dollar',
+    id: 15
+  },
+  {
+    abbr: 'BND',
+    description: 'Brunei dollar',
+    id: 16
+  },
+  {
+    abbr: 'BOB',
+    description: 'Bolivian boliviano',
+    id: 17
+  },
+  {
+    abbr: 'BRL',
+    description: 'Brazilian Real',
+    id: 18
+  },
+  {
+    abbr: 'BSD',
+    description: 'Bahamian Dollar',
+    id: 19
+  },
+  {
+    abbr: 'BTN',
+    description: 'Bhutanese Ngultrum',
+    id: 20
+  },
+  {
+    abbr: 'BWP',
+    description: 'Botswana Pula',
+    id: 21
+  },
+  {
+    abbr: 'BYN',
+    description: 'Belarusian ruble',
+    id: 22
+  },
+  {
+    abbr: 'BZD',
+    description: 'Belizean Dollar',
+    id: 23
+  },
+  {
+    abbr: 'CAD',
+    description: 'Canadian Dollar',
+    id: 24
+  },
+  {
+    abbr: 'CDF',
+    description: 'Congolese Franc',
+    id: 25
+  },
+  {
+    abbr: 'CHF',
+    description: 'Swiss Franc',
+    id: 26
+  },
+  {
+    abbr: 'CNY',
+    description: 'Chinese Yuan Renminbi',
+    id: 27
+  },
+  {
+    abbr: 'CZK',
+    description: 'Czech Koruna',
+    id: 28
+  },
+  {
+    abbr: 'DKK',
+    description: 'Danish Krone',
+    id: 29
+  },
+  {
+    abbr: 'DKK',
+    description: 'Danish Krone',
+    id: 30
+  },
+  {
+    abbr: 'EUR',
+    description: 'Euro',
+    id: 31
+  },
+  {
+    abbr: 'GBP',
+    description: 'Pound Sterling',
+    id: 32
+  },
+  {
+    abbr: 'ILS',
+    description: 'Israeli Shekel',
+    id: 33
+  },
+  {
+    abbr: 'INR',
+    description: 'Indian Rupee',
+    id: 34
+  },
+  {
+    abbr: 'JPY',
+    description: 'Japanese Yen',
+    id: 35
+  },
+  {
+    abbr: 'KRW',
+    description: 'South Korean Won',
+    id: 36
+  },
+  {
+    abbr: 'KZT',
+    description: 'Kazakhstani Tenge',
+    id: 37
+  },
+  {
+    abbr: 'MXN',
+    description: 'Mexican Peso',
+    id: 38
+  },
+  {
+    abbr: 'NOK',
+    description: 'Norwegian Krone',
+    id: 39
+  },
+  {
+    abbr: 'NZD',
+    description: 'New Zealand Dollar',
+    id: 40
+  },
+  {
+    abbr: 'RUB',
+    description: 'Russian Ruble',
+    id: 41
+  },
+  {
+    abbr: 'SEK',
+    description: 'Swedish Krona',
+    id: 42
+  },
+  {
+    abbr: 'TRY',
+    description: 'Turkish Lira',
+    id: 43
+  },
+  {
+    abbr: 'UAH',
+    description: 'Ukrainian Hryvnia',
+    id: 44
+  }
+]
+
+export default CURRENCIES;
